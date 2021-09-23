a5b169c25e3235913ca6bde5c4c6a8508bf5597d
Teemo222 <740286700@qq.com>
2021-09-23 00:01:32 -0400

    Using List instead of float array for BandPassFilterAgg

--- trill_bench/bench/BandPassFilterAgg.cs
+++ trill_bench/bench/BandPassFilterAgg.cs
@@ -1,68 +1,70 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Collections.Generic;
 using Microsoft.StreamProcessing.Aggregates;
 
 namespace Microsoft.StreamProcessing
 {
-    public struct LowPassAggState
+    public struct PassAggState
     {
-        public float[] InputStream; // Should always have size 12
-        public float[] OutputStream; // Should always have size 2 
+        public List<float> InputStream; // Should always have size 12
+        public List<float> OutputStream; // Should always have size 2 
     }
 
-    public class LowPassAgg : IAggregate<float, LowPassAggState, float>
+    public class LowPassAgg : IAggregate<float, PassAggState, float>
     {
-        public Expression<Func<LowPassAggState>> InitialState()
-            => () => new LowPassAggState{ InputStream = new float[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                          OutputStream = new float[] {0, 0}};
+        public Expression<Func<PassAggState>> InitialState()
+            => () => new PassAggState{ InputStream = new List<float>(Enumerable.Repeat(0f, 12)),
+                                       OutputStream = new List<float>(Enumerable.Repeat(0f, 2))};
 
-        public Expression<Func<LowPassAggState, long, float, LowPassAggState>> Accumulate()
-            => (oldState, timestamp, input) =>
-                new LowPassAggState {
-                    InputStream = oldState.InputStream.Skip(1).Concat(new float[] {input}).ToArray(),
-                    OutputStream = new float[] { oldState.OutputStream[1],
-                                                 2 * oldState.OutputStream[1] - oldState.OutputStream[0] + input - 2 * oldState.InputStream[6] + oldState.InputStream[0]}
-                };
+        public static PassAggState accumulate(PassAggState oldState, long timestamp, float input)
+        {
+            oldState.OutputStream.Add(2 * oldState.OutputStream[1] - oldState.OutputStream[0] + input - 2 * oldState.InputStream[6] + oldState.InputStream[0]);
+            oldState.OutputStream.RemoveAt(0);
+            oldState.InputStream.Add(input);
+            oldState.InputStream.RemoveAt(0);
+            return oldState;
+        }       
 
-        public Expression<Func<LowPassAggState, long, float, LowPassAggState>> Deaccumulate()
-            => (oldState, timestamp, input) => oldState;
+        public Expression<Func<PassAggState, long, float, PassAggState>> Accumulate()
+            => (oldState, timestamp, input) => accumulate(oldState, timestamp, input);
 
-        public Expression<Func<LowPassAggState, LowPassAggState, LowPassAggState>> Difference()
+        public Expression<Func<PassAggState, long, float, PassAggState>> Deaccumulate()
+            => (oldState, timestamp, input) => accumulate(oldState, timestamp, input);
+
+        public Expression<Func<PassAggState, PassAggState, PassAggState>> Difference()
             => (left, right) => left;
 
-        public Expression<Func<LowPassAggState, float>> ComputeResult()
+        public Expression<Func<PassAggState, float>> ComputeResult()
             => state => state.OutputStream[1];
     }
 
-    public struct HighPassAggState
+    public class HighPassAgg : IAggregate<float, PassAggState, float>
     {
-        public float[] InputStream; // Should always have size 32
-        public float Output; // Should always have size 
-    }
+        public Expression<Func<PassAggState>> InitialState()
+            => () => new PassAggState{ InputStream = new List<float>(new float[32]),
+                                       OutputStream = new List<float>(new float[1])};
 
-    public class HighPassAgg : IAggregate<float, HighPassAggState, float>
-    {
-        public Expression<Func<HighPassAggState>> InitialState()
-            => () => new HighPassAggState{ InputStream = new float[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                                                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                                                      0, 0, 0, 0, 0, 0, 0, 0},
-                                           Output = 0};
+        public static PassAggState accumulate(PassAggState oldState, long timestamp, float input)
+        {
+            oldState.OutputStream.Add(32 * oldState.InputStream[16] - (oldState.OutputStream[0] + input - oldState.InputStream[0]));
+            oldState.OutputStream.RemoveAt(0);
+            oldState.InputStream.Add(input);
+            oldState.InputStream.RemoveAt(0);
+            return oldState;
+        }  
 
-        public Expression<Func<HighPassAggState, long, float, HighPassAggState>> Accumulate()
-            => (oldState, timestamp, input) =>
-                new HighPassAggState {
-                    InputStream = oldState.InputStream.Concat(new float[] {input}).Skip(1).ToArray(),
-                    Output = 32 * oldState.InputStream[16] - (oldState.Output + input - oldState.InputStream[0])
-                };
+        public Expression<Func<PassAggState, long, float, PassAggState>> Accumulate()
+            => (oldState, timestamp, input) => accumulate(oldState, timestamp, input);
 
-        public Expression<Func<HighPassAggState, long, float, HighPassAggState>> Deaccumulate()
+        public Expression<Func<PassAggState, long, float, PassAggState>> Deaccumulate()
             => (oldState, timestamp, input) => oldState;
 
-        public Expression<Func<HighPassAggState, HighPassAggState, HighPassAggState>> Difference()
+        public Expression<Func<PassAggState, PassAggState, PassAggState>> Difference()
             => (left, right) => left;
 
-        public Expression<Func<HighPassAggState, float>> ComputeResult()
-            => state => state.Output;
+        public Expression<Func<PassAggState, float>> ComputeResult()
+            => state => state.OutputStream[0];
     }
 }
