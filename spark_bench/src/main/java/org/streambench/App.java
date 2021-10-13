6a52b0eab7fc27374980e18ae9f491fcabaeaa02
Yudi Sun <yudi.sun19@gmail.com>
2021-10-12 22:16:37 -0400

    convert Dstream to Dataframe

--- spark_bench/src/main/java/org/streambench/App.java
+++ spark_bench/src/main/java/org/streambench/App.java
@@ -7,6 +7,15 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+
 /**
  * Hello world!
  */
@@ -20,6 +29,33 @@ public class App {
 
         JavaReceiverInputDStream<Event> stream = ssc.receiverStream(new TestReceiver(period, size));
 
+        stream.foreachRDD(new VoidFunction<JavaRDD<Event>>() {
+            @Override
+            public void call(JavaRDD<Event> rdd) {
+                SparkSession spark = SparkSession.builder().config(rdd.sparkContext.getConf).getOrCreate();
+                JavaRDD<Row> rowRDD = rdd.map(new Function<Event, Row>() {
+                    @Override
+                    public Row call(Event event) {
+                      Row row = RowFactory.create(event);
+                      return row;
+                    }
+                  });
+            
+                List<StructField> fields = new ArrayList<>();
+                StructField field1 = DataTypes.createStructField("StartTime", DataTypes.TimestampType, true);
+                StructField field2 = DataTypes.createStructField("EndTime", DataTypes.TimestampType, true);
+                StructField field3 = DataTypes.createStructField("Payload", DataTypes.DoubleType, true);
+                fields.add(field1);
+                fields.add(field2);
+                fields.add(field3);
+                StructType schema = DataTypes.createStructType(fields);
+        
+                Dataset<Row> eventDataFrame = spark.createDataFrame(rowRDD, schema);
+                eventDataFrame.show();
+            }
+        });
+
+        /*
         JavaDStream<Float> res = stream.map(new Function<Event, Float>() {
             @Override
             public Float call(Event event) throws Exception {
@@ -27,6 +63,7 @@ public class App {
             }
         });
         res.print();
+        */
 
         ssc.start();
         ssc.awaitTermination();
