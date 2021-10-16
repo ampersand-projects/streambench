5f3304cf44d29f8c8f90945d3257861115a434b2
Yudi Sun <yudi.sun19@gmail.com>
2021-10-16 16:27:25 -0400

    add select, filter, join, aggregate

--- spark_bench/src/main/java/org/streambench/App.java
+++ spark_bench/src/main/java/org/streambench/App.java
@@ -7,8 +7,7 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Hello world!
@@ -26,32 +25,67 @@ class Event {
 }
 
 public class App {
+    public static Dataset<Row> generateDf(Long size, Long period, SparkSession spark) {
+        List<Row> list = new ArrayList<>();
+        Random rand = new Random();
+        double range = 100.0;
+        for (int i = 0; i < size; i++) {
+            double payload = rand.nextDouble() * range - range / 2;
+            list.add(RowFactory.create(new Timestamp(i * period), new Timestamp((i + 1) * period), (float) payload));
+        }
+        StructType schema = DataTypes.createStructType(
+                new StructField[] { DataTypes.createStructField("start_time", DataTypes.TimestampType, false),
+                        DataTypes.createStructField("end_time", DataTypes.TimestampType, false),
+                        DataTypes.createStructField("payload", DataTypes.FloatType, false) });
+
+        return spark.createDataFrame(list, schema);
+    }
+
     public static void main(String[] args) {
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("Test")
-                .getOrCreate();
+        SparkSession spark = SparkSession.builder().appName("Test").getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
 
+        /*
         List<Row> list = new ArrayList<>();
         list.add(RowFactory.create(new Timestamp(0), new Timestamp(1), 10.0f));
         list.add(RowFactory.create(new Timestamp(1), new Timestamp(2), 11.0f));
         list.add(RowFactory.create(new Timestamp(2), new Timestamp(3), 12.0f));
         list.add(RowFactory.create(new Timestamp(3), new Timestamp(4), 13.0f));
 
-        StructType schema = DataTypes.createStructType(new StructField[]{
-                DataTypes.createStructField("start_time", DataTypes.TimestampType, false),
-                DataTypes.createStructField("end_time", DataTypes.TimestampType, false),
-                DataTypes.createStructField("payload", DataTypes.FloatType, false)
-        });
+        StructType schema = DataTypes.createStructType(
+                new StructField[] { DataTypes.createStructField("start_time", DataTypes.TimestampType, false),
+                        DataTypes.createStructField("end_time", DataTypes.TimestampType, false),
+                        DataTypes.createStructField("payload", DataTypes.FloatType, false) });
 
-        Dataset<Row> stream = spark
-                .createDataFrame(list, schema);
+        Dataset<Row> stream = spark.createDataFrame(list, schema);
+        */
+        long size = 10;
+        long period = 1;
+        Dataset<Row> stream = generateDf(size, period, spark);
 
-        Dataset<Row> res = stream
-                .groupBy(functions.window(stream.col("start_time"), "2 milliseconds"))
+        /*
+         * Dataset<Row> select = stream .map(new Function<Row, Row> () {
+         * 
+         * @Override public Row call(Row row) throws Exception { return
+         * RowFactory.create(row.getTimestamp(0), row.getTimestamp(1),
+         * row.getFloat(2)+3.0f); }} );
+         */
+        // Dataset<Row> select = stream.withColumn("payload", functions.col("payload") +
+        // functions.lit(3));
+        Dataset<Row> select = stream.select(functions.col("start_time"), functions.col("end_time"),
+                functions.col("payload").plus(3.0f));
+
+        Dataset<Row> where = stream.filter("payload > 0");
+
+        Dataset<Row> aggregate = stream.groupBy(functions.window(stream.col("start_time"), "2 milliseconds"))
                 .sum("payload");
 
-        res.foreach((ForeachFunction<Row>) e -> System.out.println(e));
+        Dataset<Row> alterdur = stream.select(functions.col("start_time"),
+                functions.col("start_time").plus(functions.expr("INTERVAL 10 MILLISECONDS")), functions.col("payload"));
+
+        Dataset<Row> stream2 = generateDf(size, period, spark);
+        Dataset<Row> innerjoin = stream.join(stream2,"start_time");
+        
+        innerjoin.foreach((ForeachFunction<Row>) e -> System.out.println(e));
     }
 }
