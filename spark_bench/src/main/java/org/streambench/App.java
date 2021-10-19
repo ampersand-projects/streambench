3ec2675c9b32f5b6261d73e6e85f157c040c621a
Yudi Sun <yudi.sun19@gmail.com>
2021-10-19 18:28:12 -0400

    modify window length to match other baselines

--- spark_bench/src/main/java/org/streambench/App.java
+++ spark_bench/src/main/java/org/streambench/App.java
@@ -46,34 +46,36 @@ public class App {
         Dataset<Row> stream1 = generateDf(size, period, spark).cache();
         double runTime = 0;
         switch (benchmark) {
-            case "select":
-                Dataset<Row> select = stream1.select(functions.col("start_time"), functions.col("end_time"),
-                        functions.col("payload").plus(3.0f));
-                runTime = runTest(select);
-                break;
-            case "where":
-                Dataset<Row> where = stream1.filter("payload > 0");
-                runTime = runTest(where);
-                break;
-            case "aggregate":
-                Dataset<Row> aggregate = stream1.groupBy(functions.window(stream1.col("start_time"), "2 milliseconds"))
-                        .sum("payload");
-                runTime = runTest(aggregate);
-                break;
-            case "alterdur":
-                Dataset<Row> alterdur = stream1.select(functions.col("start_time"),
-                        functions.col("start_time").plus(functions.expr("INTERVAL 10 MILLISECONDS")),
-                        functions.col("payload"));
-                runTime = runTest(alterdur);
-                break;
-            case "innerjoin":
-                Dataset<Row> stream2 = generateDf(size, period, spark).cache();
-                Dataset<Row> innerjoin = stream1.join(stream2,
-                        stream1.col("start_time").equalTo(stream2.col("start_time")));
-                runTime = runTest(innerjoin);
-                break;
-            default:
-                System.out.println("Unknown benchmark type");
+        case "select":
+            Dataset<Row> select = stream1.select(functions.col("start_time"), functions.col("end_time"),
+                    functions.col("payload").plus(3.0f));
+            runTime = runTest(select);
+            break;
+        case "where":
+            Dataset<Row> where = stream1.filter("payload > 0");
+            runTime = runTest(where);
+            break;
+        case "aggregate":
+            Dataset<Row> aggregate = stream1.groupBy(
+                    functions.window(stream1.col("start_time"), String.format("%d milliseconds", 1000 * period)))
+                    .sum("payload");
+            runTime = runTest(aggregate);
+            break;
+        case "alterdur":
+            Dataset<Row> alterdur = stream1.select(functions.col("start_time"),
+                    functions.col("start_time")
+                            .plus(functions.expr(String.format("INTERVAL %d MILLISECONDS", 10 * period))),
+                    functions.col("payload"));
+            runTime = runTest(alterdur);
+            break;
+        case "innerjoin":
+            Dataset<Row> stream2 = generateDf(size, period, spark).cache();
+            Dataset<Row> innerjoin = stream1.join(stream2,
+                    stream1.col("start_time").equalTo(stream2.col("start_time")));
+            runTime = runTest(innerjoin);
+            break;
+        default:
+            System.out.println("Unknown benchmark type");
         }
 
         System.out.printf("Benchmark: %s; Execution Time: %.3f seconds", benchmark, runTime);
