6f230d7add5bb79d8e927c093fcc9844f1d52197
WeiZhao <740286700@qq.com>
2022-02-23 19:10:46 -0500

    load dataset by command line arguments

--- dataset_util/loader/cpp/loader.cpp
+++ dataset_util/loader/cpp/loader.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <string>
 
 #include <data_loader.h>
 #include <taxi_data_parser.h>
 
 using namespace std;
 
+template<typename T>
+void print_data(data_loader<T> &loader)
+{
+    while (true) {
+        T t;
+        if (!loader.load_data(t)) {
+            break;
+        }
+        cout << t << endl;
+    }
+}
+
 int main(int argc, char** argv)
 {
-    data_loader<stream::taxi_fare> loader;
-    while (true) {
-        stream::taxi_fare fare;
-        loader.load_data(fare);
-        cout << fare << endl;
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    
+    string dataset_name = "taxi_fare";
+    if (argc > 1) {
+        dataset_name = argv[1];
+    }
+
+    if (dataset_name == "taxi_fare") {
+        data_loader<stream::taxi_fare> loader;
+        print_data<stream::taxi_fare>(loader);
+    } else if (dataset_name == "taxi_trip") {
+        data_loader<stream::taxi_trip> loader;
+        print_data<stream::taxi_trip>(loader);
     }
     
     google::protobuf::ShutdownProtobufLibrary();
--- dataset_util/loader/csharp/loader/Program.cs
+++ dataset_util/loader/csharp/loader/Program.cs
@@ -4,14 +4,45 @@ using Google.Protobuf;
 
 namespace loader
 {
+    public class DataLoader<T> where T : IMessage<T>, new()
+    {
+        private MessageParser<T> MsgParser = new MessageParser<T>(() => new T());
+
+        public T LoadData()
+        {
+            return MsgParser.ParseDelimitedFrom(Console.OpenStandardInput());
+        }
+    }
+
     class Program
     {
+        static void PrintData<T>(DataLoader<T> data_loader) where T : IMessage<T>, new()
+        {
+            try {
+                while (true) {
+                    T t = data_loader.LoadData();
+                    Console.WriteLine(t);
+                }
+            } catch (Exception e) {
+                return;
+            }
+        }
+
         static void Main(string[] args)
         {
-            MessageParser<taxi_fare> parser = new MessageParser<taxi_fare>(() => new taxi_fare());
-            while (true) {
-                taxi_fare fare = parser.ParseDelimitedFrom(Console.OpenStandardInput());
-                Console.WriteLine(fare);
+            string dataset = "taxi_fare";
+            if (args.Length > 0) {
+                dataset = args[0];
+            }
+
+            if (dataset == "taxi_fare") {
+                DataLoader<taxi_fare> data_loader = new DataLoader<taxi_fare>();
+                PrintData<taxi_fare>(data_loader);
+            } else if (dataset == "taxi_trip") {
+                DataLoader<taxi_trip> data_loader = new DataLoader<taxi_trip>();
+                PrintData<taxi_trip>(data_loader);
+            } else {
+                throw new Exception("Unknown dataset");
             }
         }
     }
