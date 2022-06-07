f590d77972a872e24b253375ba8ef73e7d1bbe13
WeiZhao <740286700@qq.com>
2022-06-06 22:04:32 -0400

    Add num_cores as command line arguments

--- lightsaber_bench/main.cpp
+++ lightsaber_bench/main.cpp
@@ -8,10 +8,11 @@ int main(int argc, const char **argv) {
     std::string testcase = (argc > 1) ? argv[1] : "select";
     int64_t size = (argc > 2) ? atoi(argv[2]) : 40000000;
     int64_t runs = (argc > 3) ? atoi(argv[3]) : 1;
+    int64_t cores = (argc > 4) ? atoi(argv[4]) : 1;
     int64_t period = 1;
     SystemConf::getInstance().BATCH_SIZE = 200000; // This means the input_size (size * sizeof(InputSchema)) must be multiple of 200,000
     SystemConf::getInstance().CIRCULAR_BUFFER_SIZE = size * sizeof(Benchmark::InputSchema);
-    SystemConf::getInstance().WORKER_THREADS = 1;
+    SystemConf::getInstance().WORKER_THREADS = cores;
 
     std::unique_ptr<Benchmark> benchmarkQuery {};
     if (testcase == "select") {
